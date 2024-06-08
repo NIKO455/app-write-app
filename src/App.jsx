46a432af14f298847916feb9b4dddfdcc1ff6246
Bhupendra Kathayat <niko@ChuckleMate2.0>
2024-06-08 18:04:50 +0545

    used redux for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Header, Footer } from './components/index'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+
 export default function App() {
-    return (
-        <div>
-            <h1>A blog app with app-write</h1>
-        </div>
-    )
+
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getUserInfo()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div>
+      <Header />
+      <main>
+        {/* TODO: <Outlet/> */}
+      </main>
+      <Footer />
+    </div>
+
+  ) : null
+
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="text-black bg-green-400">Header</div>
+  )
+}
+
+export default Header
